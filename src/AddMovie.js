2ee5f3f832b96ac51aa4289bae8167738784daf2
Chris Bryant <chrisbryant.dev@gmail.com>
2021-03-26 00:23:33 -0400

    Adds alert if name or price form fields are empty when adding a new movie to list

--- src/AddMovie.js
+++ src/AddMovie.js
@@ -1,40 +1,57 @@
-import React, { useState, useContext } from 'react';
-import { MovieContext } from './MovieContext';
+import React, { useState, useContext } from "react";
+import { MovieContext } from "./MovieContext";
 
 const AddMovie = () => {
-    const [name, setName] = useState('');
-    const [price, setPrice] = useState('');
-    // eslint-disable-next-line
-    const [movies, setMovies] = useContext(MovieContext);
+  const [name, setName] = useState(null);
+  const [price, setPrice] = useState(null);
+  // eslint-disable-next-line
+  const [movies, setMovies] = useContext(MovieContext);
 
-    const updateName = (e) => {
-        setName(e.target.value);
-    }
+  const updateName = (e) => {
+    setName(e.target.value);
+  };
 
-    const updatePrice = (e) => {
-        setPrice(e.target.value);
-    }
+  const updatePrice = (e) => {
+    setPrice(e.target.value);
+  };
 
-    const addMovie = e => {
-        e.preventDefault();
-        // This is where we would use the updater function
-        setMovies(prevMovies => [...prevMovies, {
+  const addMovie = (e) => {
+    e.preventDefault();
+
+    name && price
+      ? // This is where we would use the updater function
+        setMovies((prevMovies) => [
+          ...prevMovies,
+          {
             name: name,
             price: price,
-            id: Math.random()
-        }])
-    }
+            id: Math.random(),
+          },
+        ])
+      : alert("Name and Price both are required.");
+  };
 
-    return (
-        <form onSubmit={addMovie}>
-            <h3 style={{width: '100%'}}>Add Movie</h3>
-            <label>Name: {` `}</label>
-            <input type="text" name="name" value={name} onChange={updateName} />
-            <label>Price: {` `}</label>
-            <input type="text" name="price" value={price} onChange={updatePrice} />
-            <button>Submit</button>
-        </form>
-    );
+  return (
+    <form onSubmit={addMovie}>
+      <h3 style={{ width: "100%" }}>Add Movie</h3>
+      <input
+        type="text"
+        name="name"
+        value={name}
+        onChange={updateName}
+        placeholder="Movie Name"
+      />
+      <label>Price: {` `}</label>
+      <input
+        type="text"
+        name="price"
+        value={price}
+        onChange={updatePrice}
+        placeholder="Price"
+      />
+      <button>Submit</button>
+    </form>
+  );
 };
 
 export default AddMovie;
--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import './App.css';
-import Nav from './Nav';
-import MovieList from './MovieList';
-import AddMovie from './AddMovie';
-import { MovieProvider } from './MovieContext';
+import "./App.css";
+import Nav from "./Nav";
+import MovieList from "./MovieList";
+import AddMovie from "./AddMovie";
+import { MovieProvider } from "./MovieContext";
 
 function App() {
   return (
